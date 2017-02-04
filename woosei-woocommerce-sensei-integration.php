61999269b1d1fc41880db68c67ba9f995da9a53f
Vinícius Lourenço <vinicius.q.lourenco@gmail.com>
2017-02-04 02:03:07 -0200

    PHPCodeSniffer fixes

--- woosei-woocommerce-sensei-integration.php
+++ woosei-woocommerce-sensei-integration.php
@@ -14,145 +14,145 @@
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
-  exit; // Exit if accessed directly.
+	exit; // Exit if accessed directly.
 }
 
 if ( ! class_exists( 'WooCommerce_Sensei_Integration' ) ) {
 
-/**
+ /**
  * Plugin main class.
  */
-class WooSei_WooCommerce_Sensei_Integration {
+	class WooSei_WooCommerce_Sensei_Integration {
+
+	/**
+	* Plugin version.
+	*
+	* @var string
+	*/
+	const VERSION = '1.0.0';
+
+	/**
+	* Instance of this class.
+	*
+	* @var object
+	*/
+
+	/**
+	* Instance of this class.
+	*
+	* @var object
+	*/
+	protected static $instance = null;
+
+	/**
+	* Check if user has the WooCommerce class active.
+	*
+	* @var bool
+	*/
+	protected $has_woo = false;
+
+	/**
+	* Check if user has the Sensei class active.
+	*
+	* @var bool
+	*/
+	protected $has_sensei = false;
 
   /**
-   * Plugin version.
-   *
-   * @var string
-   */
-  const VERSION = '1.0.0';
+	* Check if user has the WooCommerce Extra Fields For Brazil class active.
+	*
+	* @var bool
+	*/
+	protected $has_woo_extra_fields = false;
 
   /**
-   * Instance of this class.
-   *
-   * @var object
-   */
+	* Initialize the plugin.
+	*
+	*/
+	private function __construct() {
+			// Load plugin text domain.
+			if ( class_exists( 'Sensei_Main' ) && class_exists( 'WooCommerce' ) && class_exists( 'Extra_Checkout_Fields_For_Brazil' ) ) {
+				$this->includes();
+				$this->include_scripts();
+			} else {
+				if ( ! class_exists( 'Sensei_Main' ) ) {
+					$this->has_sensei = true;
+				}
+				if ( ! class_exists( 'WooCommerce' ) ) {
+					$this->has_woo = true;
+				}
+				if ( ! class_exists( 'Extra_Checkout_Fields_For_Brazil' ) ) {
+					$this->has_woo_extra_fields = true;
+				}
+				add_action( 'admin_notices', array( $this, 'fallback_notice' ) );
+			}
+		}
 
+	/**
+	* Return an instance of this class.
+	*
+	* @return object single instance of this class.
+	*/
+		public static function get_instance() {
+			if ( null == self::$instance ) {
+				self::$instance = new self;
+			}
+
+			return self::$instance;
+		}
+
+	/**
+	* Admin includes.
+	*/
+		private function includes() {
+			include_once 'includes/class-woosei-woocommerce-sense-integration.php';
+		}
+
+
+	/**
+	* Method to call the custom scripts.
+	*/
+		private function include_scripts() {
+			add_action( 'wp_enqueue_scripts', array( $this, 'woosei_custom_scripts' ) );
+		}
+
+	/**
+	* Method to register and call the scripts!
+	*/
+		public function woosei_custom_scripts() {
+			wp_register_script( 'woosei-custom-js', plugins_url( 'js/custom-js.js' , __FILE__ ) , '', false, false );
+			wp_enqueue_script( 'woosei-custom-js' );
+		}
+
+		  
   /**
-  * Instance of this class.
-  *
-  * @var object
-  */  
-  protected static $instance = null;
+	* WooSei fallback notice.
+	*
+	*Wee need some plugins to work, and if any isn't active we'll show you!
+	*/
+		public function fallback_notice() {
+			$plugins_needed = array();
+			if ( $this->has_woo ) {
+				$plugins_needed[] = '<li>WooCommerce - <a href="https://br.wordpress.org/plugins/woocommerce/">Download</a></li>';
+			}
+			if ( $this->has_sensei ) {
+				$plugins_needed[] = '<li>Sensei</li> - <a href="https://github.com/Automattic/sensei">Download</a>';
+			}
+			if ( $this->has_woo_extra_fields ) {
+				$plugins_needed[] = '<li>WooCommerce Extra Fields For Brazil - <a href="https://br.wordpress.org/plugins/woocommerce-extra-checkout-fields-for-brazil/">Download</li>';
+			}
 
-  /**
-  * Check if user has the WooCommerce class active.
-  *
-  * @var bool
-  */
-  protected $hasWoo = false;
+			echo '<div class="error"><p>Atenção, para funcionamento completo do plugin <strong>WooSei - WooCommerce Sensei Integration</strong> é necessário ativar os seguintes plugins abaixo:</p><ul>';
+			foreach ( $plugins_needed as $plugin_name ) {
+				echo $plugin_name;
+			}
+			echo '</ul></div>';
+		}
+	}
 
-  /**
-  * Check if user has the Sensei class active.
-  *
-  * @var bool
-  */
-  protected $hasSensei = false;
-
-  /**
-  * Check if user has the WooCommerce Extra Fields For Brazil class active.
-  *
-  * @var bool
-  */
-  protected $hasWooExtraFields = false;
-
-  /**
-   * Initialize the plugin.
-   *
-   */
-  private function __construct() {
-    // Load plugin text domain.    
-
-    if ( class_exists( 'Sensei_Main' ) && class_exists( 'WooCommerce' ) && class_exists( 'Extra_Checkout_Fields_For_Brazil' )) {
-      $this->includes();
-      $this->include_scripts();
-    } else {
-      if( !class_exists( 'Sensei_Main' ) ){
-        $this->hasSensei = true;
-      }
-      if( !class_exists( 'WooCommerce' ) ){
-        $this->hasWoo = true;
-      }
-      if( !class_exists( 'Extra_Checkout_Fields_For_Brazil' ) ){
-        $this->hasWooExtraFields = true;
-      }      
-      add_action( 'admin_notices', array( $this, 'fallback_notice' ) );
-    }
-  }
-
-  /**
-   * Return an instance of this class.
-   *
-   * @return object single instance of this class.
-   */
-  public static function get_instance() {    
-    if ( null == self::$instance ) {
-      self::$instance = new self;
-    }
-
-    return self::$instance;
-  }
-
-  /**
-   * Admin includes.
-   */
-  private function includes() {
-    include_once 'includes/class-woosei-woocommerce-sense-integration.php';
-  }
-
-
-  /**
-   * Method to call the custom scripts.
-   */  
-  private function include_scripts(){
-    add_action( 'wp_enqueue_scripts', array( $this, 'woosei_custom_scripts' ) );
-  }
-
-  /**
-   * Method to register and call the scripts!
-   */
-  public function woosei_custom_scripts(){
-    wp_register_script('woosei-custom-js', plugins_url( 'js/custom-js.js' , __FILE__ ) , '', false, false );
-    wp_enqueue_script('woosei-custom-js');
-  }
-
-  /**
-   * WooSei fallback notice.
-   *
-   *Wee need some plugins to work, and if any isn't active we'll show you!
-   */
-  public function fallback_notice() {
-    $plugins_needed = array();
-    if($this->hasWoo){
-      $plugins_needed[] = '<li>WooCommerce - <a href="https://br.wordpress.org/plugins/woocommerce/">Download</a></li>';
-    }
-    if($this->hasSensei){
-      $plugins_needed[] = '<li>Sensei</li> - <a href="https://github.com/Automattic/sensei">Download</a>';
-    }
-    if($this->hasWooExtraFields){
-      $plugins_needed[] = '<li>WooCommerce Extra Fields For Brazil - <a href="https://br.wordpress.org/plugins/woocommerce-extra-checkout-fields-for-brazil/">Download</li>';
-    }
-    
-    echo '<div class="error"><p>Atenção, para funcionamento completo do plugin <strong>WooSei - WooCommerce Sensei Integration</strong> é necessário ativar os seguintes plugins abaixo:</p><ul>';
-    foreach ($plugins_needed as $plugin_name){
-      echo $plugin_name;
-    }
-    echo '</ul></div>';
-  }
-}
-
-/**
+	/**
  * Initialize the plugin.
  */
-$WooSei_WooCommerce_Sensei_Integration = WooSei_WooCommerce_Sensei_Integration::get_instance();
+	$woosei_woocommerce_sensei_integration = WooSei_WooCommerce_Sensei_Integration::get_instance();
 
 }
